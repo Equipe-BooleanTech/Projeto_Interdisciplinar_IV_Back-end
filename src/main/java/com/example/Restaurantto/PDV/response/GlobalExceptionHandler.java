429fda3231c37fe18565dec1d3d57e258ac57834
Ricardo <ric_msouza@hotmail.com>
2024-10-08 00:46:28 -0300

    Update: adicionando todas as exceptions criadas

--- src/main/java/com/example/Restaurantto/PDV/response/GlobalExceptionHandler.java
+++ src/main/java/com/example/Restaurantto/PDV/response/GlobalExceptionHandler.java
@@ -1,6 +1,10 @@
 package com.example.Restaurantto.PDV.response;
 
+import com.example.Restaurantto.PDV.exception.product.IngredientNotFoundException;
+import com.example.Restaurantto.PDV.exception.product.SupplierAlreadyRegisteredException;
 import com.example.Restaurantto.PDV.exception.product.SupplierNotFoundException;
+import com.example.Restaurantto.PDV.exception.user.EmailAlreadyRegisteredException;
+import com.example.Restaurantto.PDV.exception.user.InvalidCredentialsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -21,4 +25,45 @@ public class GlobalExceptionHandler {
 
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
+
+    @ExceptionHandler(InvalidCredentialsException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidCredentialsException(InvalidCredentialsException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                ex.getMessage(),
+                HttpStatus.UNAUTHORIZED.value(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(EmailAlreadyRegisteredException.class)
+    public ResponseEntity<ErrorResponse> handleEmailAlreadyRegistered(EmailAlreadyRegisteredException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                ex.getMessage(),
+                HttpStatus.CONFLICT.value(),
+                LocalDateTime.now()
+                );
+        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(SupplierAlreadyRegisteredException.class)
+    public ResponseEntity<ErrorResponse> handleSupplierAlreadyRegistered(SupplierAlreadyRegisteredException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                ex.getMessage(),
+                HttpStatus.CONFLICT.value(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(IngredientNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleIngredientNotFoundException(IngredientNotFoundException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                ex.getMessage(),
+                HttpStatus.NOT_FOUND.value(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
 }
