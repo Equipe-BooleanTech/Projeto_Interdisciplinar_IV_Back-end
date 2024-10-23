64782b2896928828c029003b42ef8cf3105b14cc
Wandersood <mecanica.wanderson@gmail.com>
2024-10-22 21:54:22 -0300

    Adicionando o dto para grupo de fichas

---
+++ src/main/java/com/example/Restaurantto/PDV/dto/groupSheet/GetGroupSheetDTO.java
@@ -0,0 +1,9 @@
+package com.example.Restaurantto.PDV.dto.groupSheet;
+
+import com.example.Restaurantto.PDV.dto.dataSheet.DataSheetDTO;
+
+import java.util.List;
+import java.util.UUID;
+
+public record GetGroupSheetDTO(UUID id, String name, List<DataSheetDTO> datasheets) {
+}
