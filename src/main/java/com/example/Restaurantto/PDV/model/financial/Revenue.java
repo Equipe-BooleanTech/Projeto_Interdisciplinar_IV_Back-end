466b9ea95b1765eb3c34abf3a8128fa596795299
Ricardo <ric_msouza@hotmail.com>
2024-11-14 19:29:31 -0300

    UPDATE: criando novos atributos para as receitas

--- src/main/java/com/example/Restaurantto/PDV/model/financial/Revenue.java
+++ src/main/java/com/example/Restaurantto/PDV/model/financial/Revenue.java
@@ -1,5 +1,6 @@
 package com.example.Restaurantto.PDV.model.financial;
 
+import com.example.Restaurantto.PDV.enums.PaymentMethod;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -24,4 +25,16 @@ public class Revenue {
 
     private LocalDate saleDate;
 
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod paymentMethod;
+
+    private String category; // Qual tipo de pagamento, interessante para implementação do ifood posteriormente pois pode ser via restaurante, via ifood, etc
+
+    private Boolean paymentStatus; // true = pago, false = pendente
+
+    private String employee; // colocar o nome do garçom ou caixa para resgistar numero de vendas por pessoa, pensando em uma feature futura
+
+    private String orderNumber; // para poder integrar com sistema de pedidos posteriormente
 }
